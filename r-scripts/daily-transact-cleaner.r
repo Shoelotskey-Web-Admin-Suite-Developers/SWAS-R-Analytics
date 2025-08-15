9190481790a2fc4f5e394064906f3ff92ae5a50d
James Esteban <james.esteban08@gmail.com>
2025-08-15 11:32:03 +0800

    fix daily-transact-cleaner

--- r-scripts/daily-transact-cleaner.r
+++ r-scripts/daily-transact-cleaner.r
@@ -9,7 +9,7 @@ cleaned_data <- raw_data %>%
   mutate(across(where(is.character), ~ str_trim(.))) %>%
   
   # Convert date_in to MM/DD/YY
-  mutate(date_in_fmt = format(as.Date(date_in, format = "%Y-%m-%d"), "%m/%d/%y")) %>%
+  mutate(date_in_fmt = format(as.Date(date_in, format = "%d-%m-%Y"), "%m/%d/%Y")) %>%
   
   # Format time_in as HH:MM then strip leading zero
   mutate(time_in_fmt = format(strptime(time_in, format = "%H:%M:%S"), "%H:%M"),
